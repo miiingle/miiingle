add53762ff9e9e6c115f968deb9e0ed21acf2705
LM Bibera <lyndon.bibera@boltoninternational.com>
2017-08-10 16:54:32 +0800

    generated spring template project for spring data and neo4j

---
+++ POC/Neo4J/poc-neo4j/src/main/java/com/mingle2/poc/neo4j/PocNeo4jApplication.java
@@ -0,0 +1,12 @@
+package com.mingle2.poc.neo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocNeo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocNeo4jApplication.class, args);
+	}
+}
