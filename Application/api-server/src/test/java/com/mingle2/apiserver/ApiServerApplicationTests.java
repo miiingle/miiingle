5bdb8b477dbeeb4667372a955dfee6394c26c795
LM Bibera <lyndon.bibera@boltoninternational.com>
2017-08-14 16:41:03 +0800

    generated API server application

---
+++ Application/api-server/src/test/java/com/mingle2/apiserver/ApiServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mingle2.apiserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
