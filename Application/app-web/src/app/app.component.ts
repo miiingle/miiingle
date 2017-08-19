ec6dcb9b4a873c8cec9d092c22f472566401082d
LM Bibera <secret.transaction@gmail.com>
2017-08-19 18:35:12 +0800

    generated login page and extracted the main app to another component

--- Application/app-web/src/app/app.component.ts
+++ Application/app-web/src/app/app.component.ts
@@ -12,44 +12,5 @@ import * as firebase from 'firebase/app';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = environment.title;
 
-  items: FirebaseListObservable<any[]>;
-  user: Observable<firebase.User>;
-
-  constructor(db: AngularFireDatabase, public afAuth: AngularFireAuth) {
-  	this.items = db.list('/items');
-  	this.user = afAuth.authState;
-  }
-
-  login() {
-    var provider = new firebase.auth.FacebookAuthProvider();
-    provider.addScope('user_birthday');
-    provider.setCustomParameters({
-      'display': 'popup'
-    });
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
-    }).catch(function(error: Error) {
-      // Handle Errors here.
-      //var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      //var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      //var credential = error.credential;
-      // ...
-      console.log("Error: " + errorMessage)
-    });
-
-    //this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-  }
-
-  logout() {
-    this.afAuth.auth.signOut();
-  }
 }
--- Application/app-web/src/app/app.module.ts
+++ Application/app-web/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
@@ -8,13 +9,32 @@ import { environment } from '../environments/environment';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
+
+const appRoutes: Routes = [
+  { 
+    path: 'login',
+    component: LoginComponent 
+  },
+  {
+    path: '',
+    component: MainComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule, // imports firebase/database, only needed for database features
     AngularFireAuthModule // imports firebase/auth, only needed for auth features
