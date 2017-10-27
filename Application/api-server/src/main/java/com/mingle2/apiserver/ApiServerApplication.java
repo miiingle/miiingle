c2ff39e57e00ea8860f89a9c556676c799e613d0
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2017-10-27 14:41:34 +0800

    added firebase beans
    Signed-off-by: Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>

--- Application/api-server/src/main/java/com/mingle2/apiserver/ApiServerApplication.java
+++ Application/api-server/src/main/java/com/mingle2/apiserver/ApiServerApplication.java
@@ -1,12 +1,31 @@
 package com.mingle2.apiserver;
 
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ApiServerApplication {
+public class ApiServerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiServerApplication.class, args);
 	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+
+	}
+
+	@Bean
+    FirebaseDatabase firebaseDatabase() {
+        FirebaseOptions options = new FirebaseOptions().
+
+        FirebaseAuth auth = new FirebaseAuth();
+        auth.set
+    }
+
 }
