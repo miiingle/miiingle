0491478c2329e12ce2af908c1a21a652471c663f
LM Bibera <lyndon.bibera@headhuntr.io>
2020-12-26 16:58:08 +0800

    Add OpenAPI

--- POC/backend-poc/src/main/java/net/miiingle/backend/Application.java
+++ POC/backend-poc/src/main/java/net/miiingle/backend/Application.java
@@ -1,7 +1,20 @@
 package net.miiingle.backend;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Backend POC",
+                version = "1.0",
+                description = "My API",
+                license = @License(name = "Apache 2.0", url = "https://foo.bar"),
+                contact = @Contact(url = "https://gigantic-server.com", name = "Fred", email = "Fred@gigagantic-server.com")
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
