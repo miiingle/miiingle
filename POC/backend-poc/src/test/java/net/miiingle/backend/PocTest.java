1247b1ef1f26915252d560a22114789719ec7a06
LM Bibera <lyndon.bibera@headhuntr.io>
2020-12-26 10:33:43 +0800

    add demo project to givr micronaut a test run

---
+++ POC/backend-poc/src/test/java/net/miiingle/backend/PocTest.java
@@ -0,0 +1,21 @@
+package net.miiingle.backend;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class PocTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
