ba76f746ad5d9db2b75597f8c8f61d3e5f7cf543
LM Bibera <lyndon.bibera@headhuntr.io>
2021-01-10 12:30:49 +0800

    demo code show principal

--- POC/demo-security/src/main/java/com/example/IndexController.java
+++ POC/demo-security/src/main/java/com/example/IndexController.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import io.micronaut.core.annotation.Introspected;
 import io.micronaut.http.annotation.*;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
@@ -24,12 +25,34 @@ public class IndexController {
 
      */
 
+    /**
+     * curl "http://localhost:8080/hello?name=test"
+     */
     @Secured(SecurityRule.IS_ANONYMOUS)
-    @Get(uri="/index", produces="text/plain")
-    public String index() {
-        return "Example Response";
+    @Get(uri="/hello")
+    public Ping index(String name) {
+        return new Ping(name);
     }
 
+    @Introspected
+    public static class Ping {
+
+        private final String name;
+
+        public Ping(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+    }
+
+    /**
+     curl "http://localhost:8080/user" \
+     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzaGVybG9jayIsIm5iZiI6MTYxMDI1MTk3OSwicm9sZXMiOltdLCJpc3MiOiJkZW1vLXNlY3VyaXR5IiwiZXhwIjoxNjEwMjU1NTc5LCJpYXQiOjE2MTAyNTE5Nzl9.S7bXPUkDG4daCHjvi4LGV-w4k1m2gH_1cAThrHoNm8A' \
+     --verbose
+    */
     @Secured(SecurityRule.IS_AUTHENTICATED)
     @Get("/user")
     public Principal currentUser(Principal principal) {
